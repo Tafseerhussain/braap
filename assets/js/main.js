ef82e2ff3a0e08ade4df3da8c7fe2bcf49a0b734
Tafseer <tafseerhussain88@gmail.com>
2022-04-17 19:14:46 +0500

    shop technician page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,52 @@
+// PRELOADER SETTING
 $(window).on('load',function() {
   $(".preloaderBg").delay(0).fadeOut("slow");
 });
+// TOOLTIPS
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  // return new bootstrap.Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    boundary: document.body // or document.querySelector('#boundary')
+  })
+})
+// PAGE LOAD SECTION ANIMATIONS
+window.sr = ScrollReveal({
+    distance: '0px',
+    duration: 1000,
+    scale: 1,
+    easing: 'ease',
+    mobile: false,
+    reset: false,
+    viewFactor: 0.2,
+});
+ScrollReveal.debug = true;
 
+sr.reveal('.fade-in');
+
+sr.reveal('.fade-left',{origin: 'left', distance: '100px', duration: 800});
+sr.reveal('.fade-left-delay',{origin: 'left', distance: '100px', duration: 800, delay: 300});
+sr.reveal('.fade-left-delay-600',{origin: 'left', distance: '100px', duration: 500, delay: 600});
+sr.reveal('.fade-left-vf40',{origin: 'left', distance: '100px', duration: 800, viewFactor: 0.4});
+
+sr.reveal('.fade-right',{origin: 'right', distance: '100px', duration: 800});
+sr.reveal('.fade-right-delay',{origin: 'right', distance: '100px', duration: 800, delay: 300});
+sr.reveal('.fade-right-delay-600',{origin: 'right', distance: '100px', duration: 800, delay: 600});
+
+sr.reveal('.fade-bottom',{origin: 'bottom', distance: '100px', duration: 800});
+sr.reveal('.fade-bottom-delay',{origin: 'bottom', distance: '100px', duration: 800, delay: 300});
+sr.reveal('.fade-bottom-delay-600',{origin: 'bottom', distance: '100px', duration: 800, delay: 600});
+sr.reveal('.fade-bottom-vf40',{origin: 'bottom', distance: '100px', duration: 800, viewFactor: 0.4});
+
+sr.reveal('.fade-top',{origin: 'top', distance: '100px', duration: 800});
+sr.reveal('.fade-top-delay',{origin: 'top', distance: '100px', duration: 800, delay: 300});
+sr.reveal('.fade-top-delay-600',{origin: 'top', distance: '100px', duration: 800, delay: 600});
+
+sr.reveal('.slide-bottom',{origin: 'bottom', distance: '50px', duration: 800, delay: 300});
+
+//====================================================
+// HOMEPAGE START
+//====================================================
 function openNav() {
     if ($(window).width() < 1025 && $(window).width() > 767) {
         document.getElementById("how-braap-works").style.width = "70%";
@@ -14,7 +59,6 @@ function openNav() {
     $(".how-braap-works-content").delay(300).fadeIn("slow");
     $(".body-overlay").delay(0).fadeIn();
 }
-
 function closeNav() {
     $(".how-braap-works-content").delay(0).fadeOut("slow");
     setTimeout( function()  {
@@ -24,16 +68,6 @@ function closeNav() {
     $(".body-overlay").delay(500).fadeOut();
     
 }
-
-
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  // return new bootstrap.Tooltip(tooltipTriggerEl)
-  return new bootstrap.Tooltip(tooltipTriggerEl, {
-    boundary: document.body // or document.querySelector('#boundary')
-  })
-})
 
 $(function() {
   $('#pt-1').hover(function() {
@@ -137,6 +171,13 @@ $( window ).resize(function() {
 
 var resultsHeader = $('.results-header').height()+100;
 
+//====================================================
+// HOMEPAGE END --------------------------------------
+//====================================================
+
+//====================================================
+// SEARCH PAGE START
+//====================================================
 jQuery(window).scroll(function () {
     if (jQuery(this).scrollTop() > heroSliderHeight) {
         jQuery('.sticky-search-bar').fadeIn()
@@ -152,42 +193,16 @@ jQuery(window).scroll(function () {
             jQuery('.navbar .header-search').fadeOut();
         }
     }
-    
 });
+//====================================================
+// SEARCH PAGE END -----------------------------------
+//====================================================
 
-window.sr = ScrollReveal({
-    distance: '0px',
-    duration: 1000,
-    scale: 1,
-    easing: 'ease',
-    mobile: false,
-    reset: false,
-    viewFactor: 0.2,
-});
-ScrollReveal.debug = true;
 
-sr.reveal('.fade-in');
 
-sr.reveal('.fade-left',{origin: 'left', distance: '100px', duration: 800});
-sr.reveal('.fade-left-delay',{origin: 'left', distance: '100px', duration: 800, delay: 300});
-sr.reveal('.fade-left-delay-600',{origin: 'left', distance: '100px', duration: 500, delay: 600});
-sr.reveal('.fade-left-vf40',{origin: 'left', distance: '100px', duration: 800, viewFactor: 0.4});
-
-sr.reveal('.fade-right',{origin: 'right', distance: '100px', duration: 800});
-sr.reveal('.fade-right-delay',{origin: 'right', distance: '100px', duration: 800, delay: 300});
-sr.reveal('.fade-right-delay-600',{origin: 'right', distance: '100px', duration: 800, delay: 600});
-
-sr.reveal('.fade-bottom',{origin: 'bottom', distance: '100px', duration: 800});
-sr.reveal('.fade-bottom-delay',{origin: 'bottom', distance: '100px', duration: 800, delay: 300});
-sr.reveal('.fade-bottom-delay-600',{origin: 'bottom', distance: '100px', duration: 800, delay: 600});
-sr.reveal('.fade-bottom-vf40',{origin: 'bottom', distance: '100px', duration: 800, viewFactor: 0.4});
-
-sr.reveal('.fade-top',{origin: 'top', distance: '100px', duration: 800});
-sr.reveal('.fade-top-delay',{origin: 'top', distance: '100px', duration: 800, delay: 300});
-sr.reveal('.fade-top-delay-600',{origin: 'top', distance: '100px', duration: 800, delay: 600});
-
-sr.reveal('.slide-bottom',{origin: 'bottom', distance: '50px', duration: 800, delay: 300});
-
+//====================================================
+// DATAHUB PAGE START
+//====================================================
 $('.part-images-slider').slick({
     slidesToShow: 15,
     slidesToScroll: 1,
@@ -283,8 +298,14 @@ if ($(".size-tags")[0]) {
     var braapSubTags = document.querySelector('.braap-subsystem-tags');
     new Tagify(braapSubTags)
 }
+//====================================================
+// DATAHUB PAGE END ----------------------------------
+//====================================================
 
 
+//====================================================
+// SEARCH PAGE START -
+//====================================================
 if ($(".search-part-card .show-more")[0]) {
     $(".search-part-card .show-more").click(function() {
         $(".search-part-card .search-part-text span").toggleClass("show-full");
@@ -315,7 +336,6 @@ $(document).ready(function() {
 
 
 // SEARCH RESULTS PARTS PAGE JS
-
 function openPartNav() {
     if ($(window).width() < 1025 && $(window).width() > 767) {
         document.getElementById("part-sidebar").style.width = "80%";
@@ -414,5 +434,38 @@ $('.part-details-popup .popup-close').click(function() {
         $('.related-parts-popup-slider').slick('unslick');
     },500);
 })
+//====================================================
+// SEARCH RESULTS PAGE END ---------------------------
+//====================================================
 
-// ENDING OF SEARCH RESULTS PARTS PAGE JS
+
+
+// ========================================================
+// MY SHOP JS
+// ========================================================
+function openTimelineNav() {
+    if ($(window).width() < 1025 && $(window).width() > 767) {
+        document.getElementById("timeline-sidebar").style.width = "80%";
+    } else if ($(window).width() < 768) {
+        document.getElementById("timeline-sidebar").style.width = "90%";
+    } else {
+        document.getElementById("timeline-sidebar").style.width = "55%";
+    }
+    $('html').css('overflow-y', 'hidden');
+    $(".timeline-sidebar-content").delay(300).fadeIn("slow");
+    $(".body-overlay").delay(0).fadeIn();
+}
+
+function closeTimelineNav() {
+    $(".timeline-sidebar-content").delay(0).fadeOut("slow");
+    setTimeout( function()  {
+        document.getElementById("timeline-sidebar").style.width = "0";
+        $('html').css('overflow-y', 'scroll');
+    }, 500);
+    $(".body-overlay").delay(500).fadeOut();
+    
+}
+
+// ========================================================
+// MY SHOP JS END
+// ========================================================
